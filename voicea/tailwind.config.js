600ff5a416dfdb0344753531772f5b122f75c4d5
nikitabg07 <nikitagholap8706@gmail.com>
2025-04-06 15:12:43 +0530

    Update tailwind.config.js

--- voicea/tailwind.config.js
+++ voicea/tailwind.config.js
@@ -55,5 +55,9 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar"),
+  ],
 };

4d98c681d8f154c023ff6e65aee69e3a1e82886a
nikitabg07 <nikitagholap8706@gmail.com>
2025-04-06 14:20:42 +0530

    Update UploadVideo.tsx

--- voicea/app/educationalplatform/teacher/components/UploadVideo.tsx
+++ voicea/app/educationalplatform/teacher/components/UploadVideo.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "https://voicea-back-ldg3.onrender.com/api/videos";
+const API_URL = "https://lperceiva.onrender.com/api/videos";
 
 const UploadVideo = () => {
   const [title, setTitle] = useState("");

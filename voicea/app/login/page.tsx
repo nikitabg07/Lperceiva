fc2390bd5166fd567d606c7f8cfb52caf32dc669
nikitabg07 <nikitagholap8706@gmail.com>
2025-04-06 14:22:42 +0530

    Update page.tsx

--- voicea/app/login/page.tsx
+++ voicea/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://voicea-back-ldg3.onrender.com/api/auth/login", {
+      const response = await fetch("https://lperceiva.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
